cf50c1fd90569bc5744175e384c69916e8184bc0
Ella711 <allenro711@gmail.com>
2023-01-12 19:07:16 -0500

    [ADD] - Files for task0

---
+++ 0x15-infinite_multiplication/holberton.h
@@ -0,0 +1,14 @@
+#ifndef _HOLBERTON_H_
+#define _HOLBERTON_H_
+
+#include <stddef.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+int _strlen(char *s);
+int main(int argc, char *argv[]);
+int _isdigit(char *n);
+int _putchar(char c);
+
+#endif /*_HOLBERTON_H_*/
