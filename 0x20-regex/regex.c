ffad6565c7f2e67c87bc9e8e931520c9c62a0188
Ella711 <allenro711@gmail.com>
2023-08-24 08:39:46 -0500

    [ADD] - Files for new directory on regex

---
+++ 0x20-regex/regex.c
@@ -0,0 +1,30 @@
+#include "regex.h"
+/**
+ * regex_match - checks whether a given pattern matches a given string
+ * @str: string to scan
+ * @pattern: regular expression
+ *
+ * Return: 1 if the pattern matches the string, or 0 if it doesn’t
+ */
+int regex_match(char const *str, char const *pattern)
+{
+	int dot = 0, star = 0;
+
+	if (!str || !pattern)
+		return (0);
+
+	dot = *str && (*str == *pattern || *pattern == '.');
+	star = *(pattern + 1) == '*';
+
+	if (!*str && !star)
+		return (*pattern ? 0 : 1);
+
+	if (dot && star)
+		return (regex_match(str + 1, pattern) || regex_match(str, pattern + 2));
+	else if (dot && !star)
+		return (regex_match(str + 1, pattern + 1));
+	else if (star)
+		return (regex_match(str, pattern + 2));
+
+	return (0);
+}
